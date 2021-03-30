19c3162dc159cc4df9541a4e0d246b9dc33c0c6b
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-03-30 17:24:28 +0200

    Calculating prices in Js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 var calcButton = document.getElementById('calculate-btn');
 calcButton.addEventListener('click', function() {
-    alert('Funziona!');
+    //Prendo i checkbox nei quali sono presenti i prezzi degli ingredienti, quindi... takeIngredient
+    var takeIngredient = document.getElementsByClassName('add-ingredient');
+
+    //Data
+    //basicPrice è il prezzo base senza aggiunte
+    //totalPrice è basicPrice + il costo dei singoli ingredienti aggiunti
+    //ingredientPrice è il costo del singolo ingrediente
+    var basicPrice = 2;
+    var totalPrice;
+    var ingredientPrice;
+
+    for(i = 0; i < takeIngredient.length; i++) {
+        var thisIngredient = takeIngredient[i];
+        var ingredientSelected = thisIngredient.checked;
+        var ingredientPrice = parseInt(thisIngredient.value);
+        console.log(ingredientSelected, ingredientPrice);
+
+        if (ingredientSelected == true) {
+            totalPrice = basicPrice + ingredientPrice;
+        }
+
+        console.log(totalPrice);
+    }
+
 })
